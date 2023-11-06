45dc574ea823322c38625fe9e624b90302125216
rudresh tiwari <rudreshtiwari57@gmail.com>
2023-11-06 11:51:17 +0530

    rudresh

---
+++ src/test/java/Windows/ThrougthLink.java
@@ -0,0 +1,15 @@
+package Windows;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class ThrougthLink {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/");
+
+	}
+
+}
